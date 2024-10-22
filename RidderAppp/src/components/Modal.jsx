7a0cd23000806bb5eb3c562a34fb5eb1b4df7689
Rupinder-17 <rupinderrandhey90@gmail.com>
2024-10-22 20:42:45 +0530

    Add , how to open modal and add foof and drink item in list

--- RidderAppp/src/components/Modal.jsx
+++ RidderAppp/src/components/Modal.jsx
@@ -4,16 +4,17 @@ export const Modal = () => {
   const [inputValue, setInputValue] = useState("");
   const [select, setSelect] = useState("");
   const [list, setList] = useState([]);
+  const [modal, setModal] = useState(false) 
+  const [ edit, setEdit] = useState()
   console.log(list);
 
   const addValue = () => {
-    if(inputValue && select){
-    const newId = Date.now();
-    setList([...list, { data: inputValue, select: select, id: newId }]);
-    setInputValue("");
-    }
-    else{
-      alert("please enter both item and select a catagory")
+    if (inputValue && select) {
+      const newId = Date.now();
+      setList([...list, { data: inputValue, select: select, id: newId }]);
+      setInputValue("");
+    } else {
+      alert("please enter both item and select a catagory");
     }
   };
   const handleDelete = (item) => {
@@ -25,39 +26,64 @@ export const Modal = () => {
   console.log(DrinkItem);
 
   return (
-    <div>
-      <div className="text-center">
-        <input
-          type="text"
-          className="w-80 bg-slate-300"
-          onChange={(e) => {
-            setInputValue(e.target.value.trimStart());
-          }}
-          value={inputValue}
-        />
-        <select
-          name=""
-          id=""
-          className=" bg-slate-400"
-          onChange={(e) => {
-            setSelect(e.target.value);
-          }}
-          value={select}
-        >
-          <option value=""></option>
-          <option value="food">Food</option>
-          <option value="drink">Drink</option>
-        </select>
+    <div className="relative">
+      <div className="text-center ">
         <button
-          className="bg-cyan-900 text-white px-3 text-xl"
-          onClick={addValue}
+          onClick={() => {
+            setModal(!modal);
+          }}
+          className="text-center mt-5 bg-cyan-900 text-white px-3 py-2"
         >
-          Add
+          Add Items
         </button>
       </div>
+
+      {modal && (
+        <div className="border w-[30%] bg-slate-200 m-auto h-44 absolute left-[620px] -top-16">
+          <input
+            type="text"
+            className="w- bg-red-300"
+            onChange={(e) => {
+              setInputValue(e.target.value.trimStart());
+            }}
+            value={inputValue}
+          />
+          <select
+            name=""
+            id=""
+            className=" bg-slate-400"
+            onChange={(e) => {
+              setSelect(e.target.value);
+            }}
+            value={select}
+          >
+            <option value=""></option>
+            <option value="food">Food</option>
+            <option value="drink">Drink</option>
+          </select>
+          <button
+            className="bg-cyan-900 text-white px-3 text-xl"
+            onClick={addValue}
+          >
+            Add
+          </button>
+          <div className="text-center py-11">
+            <button
+              className="bg-black text-white px-6 py-1 rounded-md"
+              onClick={() => {
+                setModal(false);
+              }}
+            >
+              Close
+            </button>
+          </div>
+        </div>
+      )}
       <div className="flex justify-between ">
-        <div className="w-[35%] bg-slate-300">
-          <h1 className="text-2xl text-center ">Food Item</h1>
+        <div className="w-[35%] ">
+          <h1 className="text-2xl text-center  bg-cyan-900 text-white">
+            Food Item
+          </h1>
           <ul className="px-3">
             {FoodItem?.map((item, index) => {
               return (
@@ -70,9 +96,22 @@ export const Modal = () => {
             })}
           </ul>
         </div>
-        <div className="bg-slate-500 w-[35%]">
-          <div className=" bg-slate-300">
-            <h1 className="text-2xl text-center ">Drink Item</h1>
+        <div>
+          <h1 className="text-cyan-900 font-semibold text-2xl">
+            {" "}
+            1 ) Total FoodItems:-
+            {list.filter((item) => item.select === "food").length}
+          </h1>
+          <h1 className="text-cyan-900 font-semibold text-2xl">
+            2 ) Total DrinkItems:-
+            {list.filter((item) => item.select === "drink").length}
+          </h1>
+        </div>
+        <div className=" w-[35%]">
+          <div className="">
+            <h1 className="text-2xl text-center bg-cyan-900 text-white ">
+              Drink Item
+            </h1>
             <ul className="px-3">
               {DrinkItem?.map((item, index) => {
                 return (
