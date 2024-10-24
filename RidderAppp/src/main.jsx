b44e9266d07bc4a3257ec8b7fbb07dfcf3ff505f
Rupinder-17 <rupinderrandhey90@gmail.com>
2024-10-24 16:47:36 +0530

    change the name of file

--- RidderAppp/src/main.jsx
+++ RidderAppp/src/main.jsx
@@ -1,14 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-// import App from './App.jsx'
 import './index.css'
-import { IndexRider } from './components/IndexRider'
-// import { Modals } from 'components/Demo'
+import { IndexRider } from './components/Index'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <IndexRider/>
-    {/* <Modal/> */}
-    {/* <Modals/> */}
   </StrictMode>,
 )
